1136bdcdfd0d72a1c487ffab1f1acf3fa288f571
Ganga Nair J U <gnairju@gmail.com>
2024-01-29 00:49:57 +0530

    database changed

--- lyf/lyf/settings.py
+++ lyf/lyf/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'lyf.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'lyf',
+        'USER': 'postgres',
+        'PASSWORD' : 'ammu',
+        'HOST' : 'localhost',
+        'PORT' : '5433',
     }
 }
 
