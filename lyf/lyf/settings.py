29c6a84a1d590d9659b203cab3627bc449b4a868
Ganga Nair J U <gnairju@gmail.com>
2024-02-07 00:44:37 +0530

    whitenoise error resolved

--- lyf/lyf/settings.py
+++ lyf/lyf/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'whitenoise.runserver_nostatic',
-    'whitenoise.runserver_nostatic',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
